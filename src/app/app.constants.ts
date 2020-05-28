e8d50e3c16635f25090311d04f60f4564618df63
Ousama <elidrissi.ousama.ma@gmail.com>
2020-05-28 12:28:39 +0000

    Change the API Url

---
+++ src/app/app.constants.ts
@@ -0,0 +1,2 @@
+export const API_COVID_MOROCCO = 'https://covid-maroc.herokuapp.com/api/all';
+export const API_COVID19 = 'https://api.covid19api.com/dayone/country/morocco/status/';
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { API_COVID_MOROCCO, API_COVID19 } from '../app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,11 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get('https://morocco-covid.herokuapp.com/api/all');
+    return this.http.get(API_COVID_MOROCCO);
   }
 
   getDataByStatus(status: string): Observable<any> {
-    const Url = 'https://api.covid19api.com/dayone/country/morocco/status/' + status;
+    const Url = API_COVID19 + status;
     return this.http.get(Url);
   }
 }
